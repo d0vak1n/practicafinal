f1594af0a72de6bc10ddfa75fa19f84667577f7e
Raul <rulanxa@gmail.com>
2022-10-19 20:41:49 +0200

    apartado a y b resueltos

--- src/app/model/pais.ts
+++ src/app/model/pais.ts
@@ -1,6 +1,12 @@
 export class Pais{
 
+
+    common:string;
+    capital:string;
+    population:number;
     region:string;   // continente
+    ccn3:string; 
+
 
 
 
--- src/app/services/pais.service.ts
+++ src/app/services/pais.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient}  from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+import { Pais } from '../model/pais';
 
 // https://restcountries.com/v3.1/all
+
+const URL = environment.URLBase;
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class PaisService {
 
-  constructor() { }
+  constructor(private httpCliente:HttpClient) {}
+
+  getAll(){
+    return this.httpCliente.get<Pais[]>(URL);
+  }
+
 }
